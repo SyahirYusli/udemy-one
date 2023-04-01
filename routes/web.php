456b983c795cdfdd301a6e4d19861e66cb32768b
SyahirYusli <syahir0106@gmail.com>
2023-04-01 22:33:42 +0800

    Admin Controller file *buat file baru untuk behaviour Admin *adjust route dalam web.php supaya dia blh bukak controller punya page based on user level

--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,10 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::mddleware(['auth', 'is_admin'])->group(function(){
+    Route::get('/admin', [AdminController::class, 'index'])->name('admin_index');
+});
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
